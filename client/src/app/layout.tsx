0ade990adb8795dfb6b0a1bb88d9f3dbe483e58e
Beqa <tvildiani2001@gmail.com>
2025-03-22 13:45:50 +0100

    add

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,30 +1,15 @@
 import "./globals.css";
 import { ReactNode } from 'react';
+import { Toaster } from "react-hot-toast";
 
-export const metadata = {
-  title: 'Template',
-  description: "Welcome to Beqa Tvildiani's personal Template",
-};
-
-interface AppProps {
-  children: ReactNode;
-}
-
-export default function App({ children }: AppProps) {
+export default function App({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
-
       <main className="bg-gray-900 min-h-[85vh]">
         {children}
       </main>
-
-      <footer className="bg-gray-800 p-4 absolute b-0 w-full">
-        <div className="container mx-auto text-center">
-          <p>&copy; chxikvia.tech | Design by <a href="https://github.com/BEQSONA-cmd" className="text-blue-400">BEQSONA-cmd</a></p>
-        </div>
-      </footer>
-
+      <Toaster />
       </body>
     </html>
   );
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,24 @@
+"use client";
+import { toast } from "react-hot-toast";
+import axios from "axios";
 
 export default function Home() {
+  const handleClick = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:8080/api/hello");
+      toast.success(data.message);
+    } catch (error) {
+      toast.error("An error occurred");
+    }
+  };
   return (
     <div className="min-h-screen flex flex-col justify-center items-center">
       <main className="flex flex-col justify-center items-center">
         <h1 className="text-4xl font-bold mb-4">
           Template
         </h1>
-        <button className="text-lg shadow__btn large scrolly bg-purple-600 hover:bg-purple-700 font-black py-3 px-6 rounded-lg hover:scale-105 duration-300">
+        <button onClick={handleClick}
+        className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-md">
           HELLO BUTTON
         </button>
       </main>
